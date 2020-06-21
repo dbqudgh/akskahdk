2f090dec6b46a3fb0f3dcd191ead861597c7b67a
dbqudgh <60841247+dbqudgh@users.noreply.github.com>
2020-06-21 22:42:41 +0900

    2020-06-21

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
+//module require
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const template = require('./lib/template.js');
+const Replace = require("./lib/replace.js");
+const manaLib = require("./lib/manaLib");
 
 
-//function
-//정규표현식
-function fn(str){
-    let res;
-    res = str.replace(/[^0-9]/g,"");
-    return res
-  }
-  
-  //숫자순 정렬
-  function numberSort(array){
-    array.sort((a,b)=>{
-      return fn(a) - fn(b)
-    })
-  }
+
+//2020-06-21
+const filenamify = require('filenamify');
+const cheerio = require("cheerio");
+const request = require("request");
+
+
 
 
 app.set('view engine','ejs');
@@ -26,18 +22,15 @@ app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+//sever on
 app.listen(3000,() =>{
     console.log('start! express server on port 3000');
 });
 
-
+//folder
 app.use(express.static('public'));
 app.use(express.static('만화'));
 const testFolder = '만화';
-
-
-
 
 
 
@@ -49,48 +42,49 @@ app.get('/',(req,res) => {
 })
 
 app.get('/mana',(req,res)=>{
-    
+     
     let body = '';
     let title = '만화모음'
-
-    //전체 만화 읽음
-    const manaList = fs.readdirSync(testFolder);
-
-    for(let i = 0; i < manaList.length; i++){
-        body += `<a href="/mana${i}">${manaList[i]}<br/><br/>`;
-    }
-
-    const html = template.HTML(title,body)
-    res.send(html)
-
+    
+    //비구조화 활당 html manaList return{html:html, manaList:manaList}
+    const {html,manaList} = manaLib.bodyinnerA(body,title,testFolder);
+    res.send(html);//html file send
 
     for(let i = 0; i < manaList.length; i++){
-
         app.get(`/mana${i}`,(req,res)=>{
-
+            
+        
             body = '';
             title = `${manaList[i]}`;
-            
+
             const episodeFolder = `${testFolder}/${manaList[i]}`//경로
             
             let episode = fs.readdirSync(episodeFolder);//에피소드
             
-            numberSort(episode)//정렬
+
+            Replace.numberSort(episode)//정렬
+
+            manaLib.downEpisode(title,episode,__dirname+'/'+episodeFolder);//최신화가 있다면 다운로드 해줌
+
+            
             
             for(let h = 0; h < episode.length; h++){
 
                 
-                app.get(`/mana${i}-${h}`,(req,res)=>{//마나 episode
+                app.get(`/mana${i}-${h}`,(req,res)=>{//마나 episode 
                     
+
                     body = '';
-                    title = `${manaList[i]}${episode[h]}`;
+                    title = `${episode[h]}`;
+
+
                     
                     
                     const imgsFolder =  `${episodeFolder}/${episode[h]}`//경로
                     
                     let imgs =  fs.readdirSync(imgsFolder);//이미지
                     
-                    numberSort(imgs)//정렬
+                    Replace.numberSort(imgs)//정렬
                     
                     for(let c = 0; c < imgs.length; c++){ //이미지 업로드
                         app.get(`/${i}-${h}${c}`,(req,res)=>{
@@ -98,7 +92,7 @@ app.get('/mana',(req,res)=>{
                                 res.end(data)
                             })
                         })
-                        body += `<img style = "text-align: center;" src="/${i}-${h}${c}">`
+                        body += `<img style = "text-align: center;" src="/${i}-${h}${c}"></br>`
                     }
                     const P = `/mana${i}-${h-1}`;//이전화
                     const L = `/mana${i}`;//총리스트
@@ -128,7 +122,3 @@ app.get('/memo',(req,res)=>{
     res.sendFile(__dirname+'/public/html/memo.html')
 })
 
-
-
-
-//2020-04-09
